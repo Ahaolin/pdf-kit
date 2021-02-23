537b2dcaa9a19cabc24e10f59bebc1f18b7890fc
Ahaolin <1064716911@qq.com>
2021-02-23 18:33:15 +0800

    饼图生成

--- src/test/java/pdf/kit/ReportKit360.java
+++ src/test/java/pdf/kit/ReportKit360.java
@@ -3,6 +3,12 @@ package pdf.kit;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.exception.ExceptionUtils;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
 import pdf.kit.component.ExporterUtil;
 import pdf.kit.component.PDFHeaderFooter;
 import pdf.kit.model.TemplateBO;
@@ -11,6 +17,8 @@ import pdf.kit.util.chart.ScatterPlotChart;
 import pdf.kit.model.chart.XYLine;
 import pdf.kit.util.chart.impl.DefaultLineChart;
 
+import java.awt.*;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -94,8 +102,41 @@ public class ReportKit360 {
         System.out.println(path);
 
         // 饼状图
+        DefaultPieDataset pds = new DefaultPieDataset();
+        pds.setValue("男生数",10);
+        pds.setValue("女生数",90);
+        String filePath = "d:/pie.jpg"; //生成图片路径及图片名
+        String title = "男女生比例图"; //饼图标题
+        createPieChart(pds,filePath,title);
     }
 
+    public static void createPieChart(DefaultPieDataset pds, String filePath,String title) {
+        try {
+            // 分别是:显示图表的标题、需要提供对应图表的DateSet对象、是否显示图例、是否生成贴士以及是否生成URL链接
+            JFreeChart chart = ChartFactory.createPieChart(title, pds, false, false, true);
+            // 如果不使用Font,中文将显示不出来
+            Font font = new Font("宋体", Font.BOLD, 12);
+            // 设置图片标题的字体
+            chart.getTitle().setFont(font);
+            // 得到图块,准备设置标签的字体
+            PiePlot plot = (PiePlot) chart.getPlot();
+            // 设置标签字体
+            plot.setLabelFont(font);
+            plot.setStartAngle(new Float(3.14f / 2f));
+//            plot.setSectionPaint((Comparable) Color.BLUE,Color.RED); // 考虑自定义设置颜色——我不会
+            // 设置plot的前景色透明度
+            plot.setForegroundAlpha(0.7f);
+            // 设置plot的背景色透明度
+            plot.setBackgroundAlpha(0.0f);
+            // 设置标签生成器(默认{0})
+            // {0}:key {1}:value {2}:百分比 {3}:sum
+            plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0}({1}占{2})"));
+            // 将内存中的图片写到本地硬盘
+            ChartUtilities.saveChartAsJPEG(new File(filePath), chart, 600, 300); //600宽度，300高度
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private static List<XYLine> getTemperatureLineList() {
         List<XYLine> list = Lists.newArrayList();
